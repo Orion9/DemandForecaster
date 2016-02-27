a81d3cd3ce6f21fb467a9f53000df61412e26867
Oğuz Kerem Tural <oguz.kerem.tural@gmail.com>
2016-02-27 20:32:49 +0200

    Updated configuration and added placeholder class.

--- src/main/java/org/itu/demandforecaster/Configuration.java
+++ src/main/java/org/itu/demandforecaster/Configuration.java
@@ -1,17 +1,30 @@
 package org.itu.demandforecaster;
 
-import org.apache.ivy.core.module.descriptor.ConfigurationGroup;
 import org.apache.spark.SparkConf;
-import org.apache.spark.api.java.*;
 
 /**
  * Created by Group 1.
  */
+
 public class Configuration {
     public static Configuration sparkConfig = null;
+
+    /**
+     * Empty constructor for Configuration singleton.
+     */
     protected Configuration(){ }
 
+    /**
+     * Returns a singleton instance that encapsulates
+     * Spark settings for entire program. Instead of
+     * configuring Spark for all components, only an
+     * instance of this singleton will be enough.
+     *
+     * @return      Spark configuration
+     * @see         Configuration
+     */
     public static Configuration getSparkConfig(){
+
         if (sparkConfig == null){
             sparkConfig = new Configuration();
         }
@@ -22,6 +35,4 @@ public class Configuration {
     SparkConf config = new SparkConf()
             .setMaster("local[4]")
             .setAppName("DemandForecaster");
-
-    JavaSparkContext sc = new JavaSparkContext(config);
 }
---
+++ src/main/java/org/itu/demandforecaster/Transaction.java
@@ -0,0 +1,77 @@
+package org.itu.demandforecaster;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static java.lang.Integer.getInteger;
+
+/**
+ * Created by Group 1.
+ */
+public class Transaction {
+    private String category;
+    private String subcategory;
+    private int amount;
+    private Date date;
+
+    public Transaction(String cat, String subcat, String amount, String date) {
+        this.setCategory(cat);
+        this.setAmount(amount);
+        this.setSubcategory(subcat);
+        this.setDate(date);
+    }
+
+    public Transaction(String cat, String subcat, int amount, Date date) {
+        this.setCategory(cat);
+        this.setAmount(amount);
+        this.setSubcategory(subcat);
+        this.setDate(date);
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public void setDate(String date){
+        DateFormat dateFormat = new SimpleDateFormat();
+        try {
+            this.date = dateFormat.parse(date);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getSubcategory() {
+        return subcategory;
+    }
+
+    public void setSubcategory(String subcategory) {
+        this.subcategory = subcategory;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public void setAmount(String amount) {
+        this.amount = getInteger(amount);
+    }
+}
