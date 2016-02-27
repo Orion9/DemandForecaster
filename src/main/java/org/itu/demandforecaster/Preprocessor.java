54f697f48dfa4a589a453dd75f47630581977241
Oğuz Kerem Tural <Orion9@users.noreply.github.com>
2016-02-27 20:35:20 +0200

    Update and rename PreProcessor.java to Preprocessor.java

---
+++ src/main/java/org/itu/demandforecaster/Preprocessor.java
@@ -0,0 +1,24 @@
+package org.itu.demandforecaster;
+
+/**
+ * Created by Group 1.
+ */
+
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.sql.DataFrame;
+import org.apache.spark.sql.SQLContext;
+
+public class Preprocessor {
+    public static void main(String[] args) {
+        Configuration sparkConfig = Configuration.getSparkConfig();
+        JavaSparkContext sc = new JavaSparkContext(sparkConfig.config);
+        SQLContext sqlContext = new SQLContext(sc);
+        DataFrame df = sqlContext.read()
+                .format("com.databricks.spark.csv")
+                .option("header", "true")
+                .load("transactions.csv");
+
+        df.take(5);
+        df.printSchema();
+    }
+}
