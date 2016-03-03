2de2041407302a4ec02c7af8bda05179262d4b96
Oğuz Kerem Tural <oguz.kerem.tural@gmail.com>
2016-03-03 06:46:12 +0200

    Finished data preprocessing and cleansing. Now Spark seperates loaded data into seperate test and training data files.

--- src/main/java/org/itu/demandforecaster/DemandForecaster.java
+++ src/main/java/org/itu/demandforecaster/DemandForecaster.java
@@ -1,10 +1,17 @@
 package org.itu.demandforecaster;
 
+import org.apache.spark.sql.DataFrame;
+
 /**
  * Created by Group 1.
  */
+
 public class DemandForecaster {
     public static void main(String[] args) {
-
+        Preprocessor pp = new Preprocessor();
+        pp.loadData();
+        DataFrame debug = pp.debug;
+        pp.getTestData().printSchema();
+        pp.getTestData().show();
     }
 }
